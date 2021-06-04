1fc9c9c01bfa166b61bb989d633e72b3cedb6a9a
PaarthBarkur1 <paarthbarkur@gmail.com>
2021-06-04 11:05:38 +0530

    added hyperparameter and training classes made using pycaret

--- Files/hyperparameter.py
+++ Files/hyperparameter.py
@@ -1,77 +1,23 @@
-import yaml
-import os
-from yaml.loader import FullLoader
-import numpy as np
-import pandas as pd
-import json
-import pickle
-import pandas as pd
-from .libraries import *
-import sys
-from Files.metrics import Metrics as met
+from pycaret.classification import *
+from pycaret.regression import *
+
 class hyperparameter:
-    def optimize(model_str,modelname,userinputconfig,datapath,dataconfig,target_column,hyperparams):
-        """
-        This function in takes the string consisting of the name and the hyperparameters of the model and uses eval function to create the model.
-        Keylist is the dictionary consisting of the infomation about the user input ('subject to further changes')
-        Name is the name of the model selected (subject to future changes)
-        """
+    def __init__(self):
+        self.parameters={} ## only valid for custom models
+    
+    def tune_model_auto(self,modellist):
+        tuned_model_list=[tune_model(i) for i in modellist]  ## model list is the list of classifiers passed in an array format
+        self.model_list=tuned_model_list
+        return tuned_model_list
 
-        data=pd.read_csv(datapath)
-        ydata=data[target_column]
-        data.drop([target_column],inplace=True,axis=1)
-        xdata=data
+    def tune_model_custom(self,modellist,parameters):
+        tuned_list=[]
+        for i in range(len(modellist)):
+            tuned_model=tune_model(modellist[i],custom_grid=parameters[i])
+            tuned_list.append(tuned_model)
+        return tuned_list
 
-        with open(userinputconfig) as f:
-            userinputconfig= yaml.load(f,Loader=FullLoader)
+    
 
-        with open(dataconfig) as c:
-            dataconfig=yaml.load(c,Loader=FullLoader)
 
-        params={}
-        for model in userinputconfig:
-            if model["name"]==modelname:
-                
-                modelname=model["name"]
-                print(modelname)
-                model_type=model["type"]
-                for hyper in model["hyper"]:
-                    if hyper["ischanged"]==False:
-                        if hyper["vary"]:
-                            if hyper["type"]=="options":
-                                params[hyper["name"]]=hyper["options"]
-                            elif hyper["type"]=="bool":
-                                params[hyper["name"]]=[True,False]
-                            elif hyper["type"]=="float" or hyper["type"]=="int":
-                                if hyper["range"]["type"]=="linear":
-                                    if hyper["type"]=="int":
-                                        params[hyper["name"]]=(np.linspace(hyper["range"]["min"],hyper["range"]["max"],hyper["range"]["num_samp"])).astype(int)
-                                    else:
-                                        print(hyper)
-                                        params[hyper["name"]]=np.linspace(hyper["range"]["min"],hyper["range"]["max"],hyper["range"]["num_samp"])
-                                if hyper["range"]["type"]=="log":
-                                    if hyper["type"]=="int":
-                                        params[hyper["name"]]=(np.logspace(np.log10(hyper["range"]["min"]),np.log10(hyper["range"]["max"]),hyper["range"]["num_samp"])).astype(int)
-                                    else:
-                                        params[hyper["name"]]=(np.logspace(np.log10(hyper["range"]["min"]),np.log10(hyper["range"]["max"]),hyper["range"]["num_samp"]))
-                        else:
-                            if hyper["type"]=="option":
-                                params[hyper["name"]]=hyper["options"]
-        model=eval(model_str)
-        sys.stdout=open("logs.log","a+")
-        clf=RandomizedSearchCV(model, params,verbose=51,n_jobs=-1)
-        
-        x_train,x_test,y_train,y_test=train_test_split(xdata,ydata,test_size=0.2)
-        print("working on "+ modelname)
-        clf.fit(x_train,y_train)
-        hyperparams[model_str]=clf.best_params_
-        print("model completed for " + modelname)
-        
-        location=os.path.join(dataconfig["location"],str(dataconfig["id"])+"_model")
-        picklepath=os.path.join(location,(str(modelname) +".pkl"))
-        with open(picklepath,"wb") as f:
-            pickle.dump(clf,f)
-            
-        prediction=clf.predict(x_test)
-        metricsrow=met.calculate_metrics(modelname,model_type,prediction,y_test)
-        return metricsrow, hyperparams
+
--- Files/training.py
+++ Files/training.py
@@ -1,110 +1,17 @@
-import yaml
-from yaml.loader import FullLoader
-import numpy as np
-import pandas as pd
-import json
-import pandas as pd
-from .libraries import *
-import yaml
-from Files.hyperparameter import hyperparameter as hp
-import os 
-import pickle
-import random
-import plotly.express as px
-import plotly.graph_objects as go
+from pycaret.classification import *
+from pycaret.regression import *
 
 class training:
+    def __init__(self):
+        self.name=""
+    def top_models_auto(self,n): # n is the number of top models we need to return according to the user
+        top_nmodels=compare_models(n_select=n)
+        return top_nmodels
 
-    def train(self,userinputconfig,dataconfig,preprocessconfig,cleanDataPath):
-        
-        with open(preprocessconfig) as f:
-            preprocessconfigfile= yaml.load(f,Loader=FullLoader) #for split ratio
+    def top_models_custom(self,models_name_list): #names of the model in pycaret conventions
+        modellist=[]
+        for model_name in models_name_list:
+            new_model=create_model(model_name)
+            modellist.append(new_model)
 
-
-        with open(dataconfig) as f:
-            dataconfigfile= yaml.load(f,Loader=FullLoader) #has info about where the data is stored and where the model must be stored
-
-        with open(userinputconfig) as file:
-            userinputconfigfile=yaml.load(file,Loader=FullLoader) #modified version of model universe for each run
-        models=[]
-        ans=[]
-        hyperparams={}
-
-        test_ratio=preprocessconfigfile["split_ratio_test"] #input given the the user usually 0.3 by default
-
-        # data=dataconfigfile["clean_data"] 
-        datapath=cleanDataPath
-
-        target_column=preprocessconfigfile["target_column_name"]
-        
-        
-        if dataconfigfile["problem_type"]=='classification':
-            metrics=pd.DataFrame(columns = ['modelname','Accuracy','Recall','Prec.','F1','Kappa'])
-
-        elif dataconfigfile["problem_type"]=='regression':
-            metrics=pd.DataFrame(columns=['modelname','MAE','MSE',"RMSE",'R2','RMSLE',"AUC"])
-        #create location of pickle file
-        picklelocation=os.path.join(dataconfigfile["location"],str(dataconfigfile["id"])+"_model")
-        os.makedirs(picklelocation)
-        #creates a pandas dataframe to store the metrics of the created model
-        for model in userinputconfigfile:
-            if model["isSelected"]:
-
-                hypers=[]
-                keylist=[]
-                for feature in model["hyper"]:
-                    if feature["ischanged"]:
-                        keylist.append(feature["name"])
-                        hypers.append(feature["name"]+"="+ str(feature["value"]))
-                model_str=model["name"] + "(" + ", ".join(hypers) + ")"
-
-                metricsnewrow, hyperparams=hp.optimize(model_str,model["name"],userinputconfig,datapath,dataconfig,target_column,hyperparams)
-                print(metricsnewrow)
-                metrics.loc[len(metrics.index)]=metricsnewrow
-                
-        #stores the metrics in the assigned folder
-        accuracy=''
-        if dataconfigfile["problem_type"]=='classification':
-            metrics=metrics.sort_values(['accuracy_score', 'f1_score'], ascending=[False, False]).reset_index()
-            accuracy=metrics['accuracy_score'][0]*100
-        else:
-            metrics=metrics.sort_values(['r2_score', 'mean_absolute_error'], ascending=[False, False]).reset_index()      
-            accuracy=metrics['r2_score'][0]
-            
-        metrics=metrics.rename(columns={"modelname":"Model"}) 
-        metricsLocation=os.path.join(dataconfigfile["location"],"metrics.csv")
-        metrics.to_csv(metricsLocation, index=False)
-        
-        # bestmodel
-        best_model=metrics['Model'][0]
-        best_model_location=os.path.join(picklelocation,(str(best_model) +".pkl"))
-        
-        hyper=hyperparams[best_model]
-        return {
-            "Successful":True,
-            "metricsLocation":metricsLocation,
-            "pickleFolderPath": picklelocation,         #Generate a folder where all pickle files are residing
-            "pickleFilePath": best_model_location,             #Best model pickle file path
-            "accuracy":accuracy,                         #Accuracy of best model
-            "cleanDataPath":cleanDataPath,
-            "clusterPlotLocation": "clusterPlotLocation",    #Only if it is clustering
-            "hyperparams":hyper
-        }
-
-    def model_plot(self,pickleFileLocation,cleandatapath,target_column,plotLocation):
-        clf=pickle.load(open(pickleFileLocation,"rb"))
-        data=pd.read_csv(cleandatapath)
-        y=data[target_column]
-        data.drop([target_column],inplace=True,axis=1)
-        x=data
-        y_pred=clf.predict(x)
-        fig = go.Figure()
-        ran=random.randint(100,999)
-        fig.add_trace(go.Scatter(x=x.index,y=y,name="actual"))
-        fig.add_trace(go.Scatter(x=x.index,y=y_pred,name="predictions"))
-        
-        plotlocation=os.path.join(plotLocation,"plot.html")
-        with open(plotlocation, 'a') as f:
-            f.write(fig.to_html(include_plotlyjs='cdn',full_html=False))
-        f.close()
-        return plotlocation
+        return modellist
