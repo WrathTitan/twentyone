981eeb9e92ff9714c358d48a9087c7ddd3f9ebe6
Nikhil Agarwal <nikhilagarwalassisi@gmail.com>
2021-06-22 22:56:45 +0530

    sepearted sections from home page

--- Frontend/pr21/src/components/section4.js
+++ Frontend/pr21/src/components/section4.js
@@ -5,20 +5,19 @@ class Section4 extends Component {
         return (
             <div className="section4" id="section4">
 
-                <div className="col-50 section4col1">
+                    <div className="col-50 section4col1">
 
-                {/* <Link to='/#section3' > <button className=" section4button">See Demo</button></Link> */}
+                        {/* <Link to='/#section3' > <button className=" section4button">See Demo</button></Link> */}
+
+                    </div>
+                    <div className="col-50 section4col2">
+                        <h1>Curl AutoMl Engine lets you make excellent machine and deep learning models for all your needs with few clicks</h1>
+
+                        <h3>"The best Part is its Open Source"</h3>
+                        <a href='https://github.com/nikzagarwal/Project_21' target="_blank" rel="noopener noreferrer"> <button className=" section4button">Github Repo</button></a>
+                    </div>
 
                 </div>
-                <div className="col-50 section4col2">
-                    <h1>Curl brings to you TwentyOne that lets you create excellent machine learning models
-                         for all your business needs with few simple clicks!</h1>
-                    <br />
-                    <h3>"The best part is that it is Open Source"</h3>
-                    <a href='https://github.com/nikzagarwal/Project_21' target="_blank" rel="noopener noreferrer"> <button className="btn btn-primary section4button">Github Repo</button></a>
-                </div>
-
-            </div>
         );
     }
 }
