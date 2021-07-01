5c056b254b1d6710f78cc509847f225f3492ad3e
Nikhil Agarwal <nikhilagarwalassisi@gmail.com>
2021-07-01 11:59:06 +0530

    allprojects section added with few issues and api calls structure changed

--- Frontend/pr21/src/components/section7.js
+++ Frontend/pr21/src/components/section7.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
-import ProjectsSection6 from './projectsSection6.js';
-import ProjectsSection5 from './projectsSection5.js';
+import Section6 from './section6.js';
+import Section5 from './section5.js';
 import $ from 'jquery';
-import axios from 'axios';
-
 class Section7 extends Component {
     constructor(props) {
         super(props)
@@ -15,50 +13,36 @@ class Section7 extends Component {
                 "dataID": 0,
                 "modelID": 0,
                 "projectID": 0,
-                "modelType": "regression"
             },
             projectList: {
                 0: {
-                    "projectID": 45547,
-                    "projectName": "Please Create your first project",
-                    "target": "NONE",
-                    "modelType": "NONE",
-                    "listOfDataIDs": [13235, 65526],
-                    "accuracies": [97, 89],
+                    "projectID": 63479,
+                    "projectName": "Nik",
+                    "target": "y",
+                    "modelType": "regression",
+                    "listOFDataIDs":{0:65025, 2:51356, 3:32541},
                     "isAuto": true
                 },
-            },
-            emptyProject: true
+                1: {
+                    "projectID": 66655,
+                    "projectName": "Nik2",
+                    "target": "y2",
+                    "modelType": "classification",
+                    "listOFDataIDs": {0:90662, 5:45452, 6:54152},
+                    "isAuto": true
+                },
+                2: {
+                    "projectID": 3,
+                    "projectName": "Nik3",
+                    "target": "y3",
+                    "modelType": "regression",
+                    "listOFDataIDs": [7, 8, 9],
+                    "isAuto": false
+                }
+
+            }
         }
-        this.changeChild = React.createRef()
-    }
 
-    componentDidMount() {
-        axios.get('http://localhost:8000/getAllProjects?userID=101')
-            .then((response) => {
-                // console.log(response.data)
-                if (response.data.length !== 0) {
-                    this.setState({
-                        projectList: response.data,
-                        emptyProject: false
-                    });
-
-                }
-            });
-
-    }
-    handleRefreshProject = event => {
-        axios.get('http://localhost:8000/getAllProjects?userID=101')
-            .then((response) => {
-                console.log(response.data)
-                if (response.data.length !== 0) {
-                    this.setState({
-                        projectList: response.data,
-                        emptyProject: false
-                    });
-
-                }
-            });
     }
     handleProjectResult = event => {
         this.setState(
@@ -69,90 +53,53 @@ class Section7 extends Component {
         $(theSectionItself).show();
         var theSectionItself2 = document.getElementById('projectDetails');
         $(theSectionItself2).hide();
-
-        // this.handleModelDetails(event.target.value)
-
-    }
-    handleModelDetails = (event, modelnumber) => {
-        console.log(event, " ", modelnumber)
-        console.log(this.state.projectList[event])
+        
+        
         this.setState({
-            currentProjectDetails: {
-                "dataID": this.state.projectList[event].listOfDataIDs[modelnumber],
-                "modelID": this.state.projectList[event].listOfDataIDs[modelnumber],
-                "projectID": this.state.projectList[event].projectID,
-                "modelType": this.state.projectList[event].modelType
+            currentProjectDetails:{
+                "dataID":this.state.projectList[event.target.value].listOFDataIDs[0][0],
+                "modelID":this.state.projectList[event.target.value].listOFDataIDs[0][0],
+                "projectID":this.state.projectList[event.target.value].projectID
             }
         })
     }
-    handleGoBack = event => {
+    handleGoBack =event=> {
         var theSectionItself = document.getElementById('modelDetails');
         $(theSectionItself).hide();
         var theSectionItself2 = document.getElementById('projectDetails');
         $(theSectionItself2).show();
-        var theFormItself = document.getElementById('projectsection6');
+        var theFormItself = document.getElementById('section6');
         $(theFormItself).show();
 
-        var theFormItself2 = document.getElementById('projectsection5');
+        var theFormItself2 = document.getElementById('section5');
         $(theFormItself2).hide();
 
         var thebtnItself = document.getElementById('show');
         $(thebtnItself).show();
-        // this.child.method()
-        this.changeChild.current.method()
         // this.setState({ reset: 1});
-    }
-    handleDeleteProject=(val) => event => {
-        let projectID=this.state.projectList[val].projectID
-        let userID=101
-        var answer = window.confirm("Confirm Delete Project?");
-        if (answer) {
-            axios.delete('http://localhost:8000/deleteThisProject/' +userID+'/'+ projectID)
-            .then((response) => {
-                this.handleRefreshProject()
-            })
-        }
-        else {
-            console.log("No")
-        }
     }
     render() {
         const items = []
         let len = Object.keys(this.state.projectList).length;
-        // let len=3
         // console.log(len)
-
         for (let i = 0; i < len; i += 3) {
             let item = []
             for (let j = i; j < i + 3 && j < len; j++) {
                 item.push(
-                    <div key={j} className="card sec7card">
+                    <div className="card sec7card">
 
                         <div className="card-body">
-                            <div className="sec7h2">
-                                <i class="fa fa-trash"  onClick={this.handleDeleteProject(j)}></i>
-                                <h2 className="card-title sec7text">{this.state.projectList[j].projectName} </h2>
-
-                            </div>
-                            <div className="sec7h5">
-                                <table className="projecttable">
-                                    <tbody>
-                                        <tr>
-                                            <td className="firstcol">Target</td>
-                                            <td> <em>{this.state.projectList[j].target} </em></td>
-                                        </tr>
-                                        <tr>
-                                            <td className="firstcol">Type</td>
-                                            <td> <em>{this.state.projectList[j].modelType}</em></td>
-                                        </tr>
-                                        <tr>
-                                            <td className="firstcol"> Mode</td>
-                                            <td> <em>{this.state.projectList[j].isAuto ? "Auto" : "Manual"}</em></td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                            <button value={j} onClick={this.handleProjectResult} className="btn sec7btn btn-primary">See Details</button>
+                            <h2 className="card-title sec7text">{this.state.projectList[j].projectName}</h2>
+                            <h5 className="card-text cardp sec7text">
+                                Target Variable :{this.state.projectList[j].target}
+                            </h5>
+                            <h5 className="card-text cardp sec7text">
+                                Model Type :{this.state.projectList[j].modelType}
+                            </h5>
+                            <h5 className="card-text cardp sec7text">
+                                Creation type :{this.state.projectList[j].isAuto ? "Auto" : "Manual"}
+                            </h5>
+                            <button value={j} onClick={this.handleProjectResult} className="btn sec7btn">See Details</button>
                         </div>
                     </div>
 
@@ -165,74 +112,31 @@ class Section7 extends Component {
             )
 
         }
-        if (this.state.emptyProject === false) {
-            return (
-
-                <div className="section7" id="section7">
-                    <div className="projectDetails" id="projectDetails">
-                        <div className=" sec7heading">
-                            <button className="btn btn-primary " onClick={this.handleRefreshProject}  >Refresh</button>
-
-                            <h2>List of all your Projects</h2>
-                        </div>
-
-                        {items}
+        return (
+            <div className="section7" id="section7">
+                <div className="projectDetails" id="projectDetails">
+                    <div className=" sec7heading">
+                        <h2>List of all the Projects you have trained</h2>
                     </div>
 
-                    <div className="modelDetails" id="modelDetails">
+                    {items}
+                </div>
+                <div className="modelDetails" id="modelDetails">
 
-                        <div className="goback">
-                            <button className="backbtn btn btn-primary" onClick={this.handleGoBack}  > &larr; Projects </button>
+                    <div className="goback">
+                        <button className="backbtn" onClick={this.handleGoBack}  >&lArr; Go Back to Projects </button>
 
-                        </div>
-                        <div id="sec6">
-                            < ProjectsSection6
-                                handler={this.props.handler}
-                                handleModelDetails={this.handleModelDetails}
-                                modelnum={this.state.projectList[this.state.currentProject].listOfDataIDs.length}
-                                isauto={this.state.projectList[this.state.currentProject].isAuto}
-                                projectname={this.state.projectList[this.state.currentProject].projectName}
-                                Accuracies={this.state.projectList[this.state.currentProject].accuracies}
-                                hyperparams={this.state.projectList[this.state.currentProject].listOfHyperparams}
-                                currentproject={this.state.currentProject}
-                                mtype={this.state.projectList[this.state.currentProject].modelType} />
-                        </div>
-                        <div id="sec5">
-                            < ProjectsSection5
-                                ref={this.changeChild}
-                                showRetrain={this.state.showRetrain}
-                                currentmodel={this.props.currentmodel}
-                                isauto={this.state.projectList[this.state.currentProject].isAuto}
-                                projectdetails={this.state.currentProjectDetails}
-                                hyperparams={this.state.projectList[this.state.currentProject].listOfHyperparams}
-                                 />
-                        </div>
+                    </div>
+                    <div id="sec6">
+                    < Section6  handler={this.props.handler} isauto={this.state.projectList[this.state.currentProject].isAuto} projectname={this.state.projectList[this.state.currentProject].projectName} />
+                   </div>
+                   <div id="sec5">
+                    < Section5 showRetrain={this.state.showRetrain} currentmodel={this.props.currentmodel} projectdetails={this.state.currentProjectDetails} />
                     </div>
                 </div>
+            </div>
 
-            );
-        }
-
-        else {
-            return (
-                <div className="section7" id="section7">
-                    <div className="projectDetails" id="projectDetails">
-                        <div className=" sec7heading">
-                            <button className="btn btn-primary " onClick={this.handleRefreshProject}  >Refresh</button>
-
-                            <h2>List of all your Projects</h2>
-                        </div>
-                        <div className="card sec7card">
-                            <h4 className="emptyproject">Either server is not started or you don't have any previous project</h4>
-                            <div className="text-center btnDiv">
-                                <a href='#section2' > <button className="btn btn-primary">Create New Project</button></a>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-            );
-        }
+        );
     }
 }
 
